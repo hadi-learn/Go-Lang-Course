46535bf2f4443610236d1ff810eff9423913be06
Hadi <hadilearn01@gmail.com>
2022-03-07 08:36:28 +0700

    add comments

--- import.go
+++ import.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	helper.SayHello("Syukri")
-	// helper.sayGoodbye("Syukri")
+	// helper.sayGoodbye("Syukri") --- tidak bisa di import karena dimulai dengan huruf kecil
 	fmt.Println(helper.Application)
-	// fmt.Println(helper.version)
+	// fmt.Println(helper.version) --- tidak bisa di akses karena dimulai dengan huruf kecil
 }
